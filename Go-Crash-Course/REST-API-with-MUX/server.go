c65db92937fb6d985585a4644e464edab026316d
Param-Scalent <param@scalent.io>
2022-04-29 10:18:50 +0530

    added Dockerfile and excutable

--- Go-Crash-Course/REST-API-with-MUX/server.go
+++ Go-Crash-Course/REST-API-with-MUX/server.go
@@ -1,8 +1,7 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"os"
 
 	"github.com/Param-Scalent/Go_Practice/Go-Crash-Course/REST-API-with-MUX/controller"
 	router "github.com/Param-Scalent/Go_Practice/Go-Crash-Course/REST-API-with-MUX/http"
@@ -14,16 +13,13 @@ var (
 	postRepository repository.PostRepository = repository.NewFirestoreRepository()
 	postService    service.PostService       = service.NewPostService(postRepository)
 	postController controller.PostController = controller.NewPostController(postService)
-	httpRouter     router.Router             = router.NewChiRouter()
+	httpRouter     router.Router             = router.NewMuxRouter()
 )
 
 func main() {
-	const port string = ":8000"
-	httpRouter.GET("/", func(resp http.ResponseWriter, req *http.Request) {
-		fmt.Fprintln(resp, "Up and Running...")
-	})
+
 	httpRouter.GET("/posts", postController.GetPosts)
 	httpRouter.POST("/posts", postController.AddPost)
 
-	httpRouter.SERVE(port)
+	httpRouter.SERVE(os.Getenv("PORT"))
 }
